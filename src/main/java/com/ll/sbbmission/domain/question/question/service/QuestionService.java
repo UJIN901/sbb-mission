0fc4fc10a97babf2bc5d67d6144f095962a63f8e
UJIN901 <dalvit9655@gmail.com>
2023-11-22 14:41:01 +0900

    add 2-10

--- src/main/java/com/ll/sbbmission/domain/question/question/service/QuestionService.java
+++ src/main/java/com/ll/sbbmission/domain/question/question/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.ll.sbbmission.domain.question.question.service;
 
 import com.ll.sbbmission.domain.question.question.entity.Question;
+import com.ll.sbbmission.domain.question.question.service.exception.DataNotFoundException;
 import com.ll.sbbmission.domain.question.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
