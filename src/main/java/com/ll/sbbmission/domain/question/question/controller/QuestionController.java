83315c4d07c16b6b120f55fb8e3e72bc8fddc314
UJIN901 <dalvit9655@gmail.com>
2023-11-22 13:28:25 +0900

    add 2-9

--- src/main/java/com/ll/sbbmission/domain/question/question/controller/QuestionController.java
+++ src/main/java/com/ll/sbbmission/domain/question/question/controller/QuestionController.java
@@ -1,7 +1,7 @@
 package com.ll.sbbmission.domain.question.question.controller;
 
 import com.ll.sbbmission.domain.question.question.entity.Question;
-import com.ll.sbbmission.domain.question.question.repository.QuestionRepository;
+import com.ll.sbbmission.domain.question.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,11 +12,11 @@ import java.util.List;
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model){
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
     }
---
+++ src/main/java/com/ll/sbbmission/domain/question/question/service/QuestionService.java
@@ -0,0 +1,18 @@
+package com.ll.sbbmission.domain.question.question.service;
+
+import com.ll.sbbmission.domain.question.question.entity.Question;
+import com.ll.sbbmission.domain.question.question.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList(){
+        return this.questionRepository.findAll();
+    }
+}
