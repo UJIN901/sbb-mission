7f523446bc76c8d3358403745a763c8dd901d59e
UJIN901 <dalvit9655@gmail.com>
2023-11-30 20:48:14 +0900

    feat: add 3-6

---
+++ src/main/java/com/ll/sbbmission/domain/user/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ll.sbbmission.domain.user.user.repository;
+
+import com.ll.sbbmission.domain.user.user.entity.SiteUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+
+}
